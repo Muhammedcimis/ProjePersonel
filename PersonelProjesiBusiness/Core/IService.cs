a9e64ec3a5500c26460c612e08bc5d36736e063d
Muhammedcimis <89448408+Muhammedcimis@users.noreply.github.com>
2024-08-25 20:26:04 +0300

    Update IService.cs

--- PersonelProjesiBusiness/Core/IService.cs
+++ PersonelProjesiBusiness/Core/IService.cs
@@ -7,14 +7,7 @@ using System.Threading.Tasks;
 namespace PersonelProjesiBusiness.Core
 {
     public interface IService<T> where T : class
-    {
-        //Servis Katmanımız için geçerli olam core -business katmanında oluşturduk ?
-        //service katmanı Bu katman business logic kodlarımın olduğu yerdir. Datayı aldıktan sonra datayla ilgili ekstra işlemlerim burada olacak
-        //Service katmanı için burası kullanılacak
-
-        //buradaki asıl amaç repository katmanından almış olduğumuz dayayı service katmanında mapping alidations, exceptions gibi veya başka şeylere dönüştürme gibi işlevleri kullanmak için
-        //örnek : mesela Personel için ayrı bir repostory oluşturudğumuzda daah sonra IService ile IGenericRepository un dönüş tipleri farklılaşmaya başlayacaktır
-
+    {        
         Task<List<T>> GetAllAsync(); // Tüm kayıtları getirir.
 
         Task<T> GetById(int id);  // Id'ye göre kayıt getirir.
